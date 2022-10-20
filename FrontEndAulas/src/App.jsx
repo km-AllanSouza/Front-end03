ef292f7969363402d22c363fd5c5fa4152fddb3c
Allan.Souza <km.allansouza@gmail.com>
2022-10-19 23:55:43 -0300

    progeto base para aulas

---
+++ FrontEndAulas/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+import { SegundaAula } from './aulas/SegundaAula/pages/Login'
+
+
+function App() {
+  
+
+  return (
+    <SegundaAula/>
+  )
+}
+
+export default App
---
+++ FrontEndAulas/src/aulas/SegundaAula/pages/Login/index.jsx
@@ -0,0 +1,25 @@
+import'./style.scss'
+
+export function SegundaAula(){
+    return ( 
+        <main>
+            <h1>Segunda Aula</h1>
+            <h2>Projeto Selecionado: NetFlix</h2>
+            <ul>
+                <li>
+                    <p>Brasil: top 10</p>
+                    
+                </li>
+                <li>
+                    <p>botao mais informações/Assistir</p>
+                </li>
+                <li>
+                    <p>Carrocel</p>                    
+                </li>
+                <li>
+                    <p>atração principal</p>                    
+                </li>
+            </ul>
+        </main>
+     )
+}
